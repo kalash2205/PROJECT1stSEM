dcdf92a760faeab71304e92985db561a3d00a049
Tushar Agarwal <76201310+Tushar49@users.noreply.github.com>
2021-02-05 12:10:53 +0530

    MinorUpdate
    menu.h started, mysql.h imported

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "menu.h"
+#include <mysql.h>
 
+#define name_len 256
 int main()
 {
     printf("\033[1m\t\t\033[7mWELCOME TO PUZZLES!!\033[0m");
--- menu.h
+++ menu.h
@@ -2,6 +2,6 @@ int trial()
 {
     enum countryFood {Indian= 1, Chinese};
     enum countryFood n1= Indian;
-    printf("%s", n1);
+    printf("%d", n1);
     return 0;
 }
