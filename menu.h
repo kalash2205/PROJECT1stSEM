832c1c90c4d224c99d342612ffb3959d852ee42a
Tushar Agarwal <76201310+Tushar49@users.noreply.github.com>
2021-02-05 22:55:57 +0530

    small drawback

--- menu.h
+++ menu.h
@@ -1,7 +1,8 @@
 int trial()
 {
-    enum countryFood {Indian= 1, Chinese, French, Italian, Japanese, Drinks, Starters, Miamese};
-    enum countryFood n1= Indian;
-    scanf("%s", n1);
+    enum countryFood {Starters= 1, Indian, Chinese, French, Pasta, Pizzas, Drinks};
+    enum countryFood n1;
+    scanf("%[a,b,c,d,....,z]", n1);
+    printf("Your choice is: %s", n1);
     return 0;
 }
---
+++ sqlConnection.h
@@ -0,0 +1,45 @@
+    // printf("Helo --- 1");
+    // MYSQL *con = mysql_init(NULL);
+    // if (con==NULL)
+    // {
+    //     printf("%s\n", mysql_error(con));
+    // }
+    // if (mysql_real_connect(con, "localhost", "tushar", "rahsut", "sample", 0, NULL, 0)== NULL)
+    // {
+    //     printf("%s\n", mysql_error(con));
+    //     mysql_close(con);
+    //     exit(1);
+    // }
+    // //
+    // //if(mysql_query(con, "DROP TABLE IF EXISTS cars"))
+    // //{
+    // //    printf("Selected");
+    // //}
+    // //if(mysql_query(con, "CREATE table cars(id int primary key auto_increment, name varchar(255), price int)"))
+    // //{
+    // //    printf("Selected");
+    // //}
+    
+    // if(mysql_query(con, "SELECT * FROM employees"))
+    // printf("Error Query Select");
+
+    // MYSQL_RES *result = mysql_store_result(con);
+    // if(result==NULL)
+    // printf("Result NULL ERROR");
+
+    // int numOfFields= mysql_num_fields(result);
+    // printf("num fields %d", numOfFields);
+    // MYSQL_ROW row;
+    // while(row=mysql_fetch_row(result))
+    // {
+    //         printf("1");
+
+    //     for(int i=0; i< numOfFields; i++)
+    //     {
+    //         printf("%s", row[i]);
+    //     }
+    //     printf("\n");
+    // }
+    // mysql_free_result(result);
+    // mysql_close(con);
+    // exit(0);
