2683d1c45296945580bcaaf6fe7ad9e8d42038d9
Tushar Agarwal <76201310+Tushar49@users.noreply.github.com>
2021-02-10 00:12:19 +0530

    Useless(IGNORE)
    Created mySQLConnection.h which isnt working.

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "menu.h"
-int main()
+
 {
-    printf("\033[1m\t\t\033[7mWELCOME TO PUZZLES!!\033[0m\n");
-    trial();
+        printf("\033[1m\t\t\033[7mWELCOME TO PUZZLES!!\033[0m\n");
 }
--- menu.h
+++ menu.h
@@ -1,8 +1,8 @@
-int trial()
+void upperLayerMenu()
 {
-    enum countryFood {Starters= 1, Indian, Chinese, French, Pasta, Pizzas, Drinks};
-    enum countryFood n1;
-    scanf("%[a,b,c,d,....,z]", n1);
-    printf("Your choice is: %s", n1);
-    return 0;
+    // enum countryFood {Starters= 1, Indian, Chinese, French, Pasta, Pizzas, Drinks};
+    // enum countryFood n1;
+    // scanf("%[a,b,c,d,....,z]", n1);
+    // printf("Your choice is: %s", n1);
+    
 }
