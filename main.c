55f18698da059873e19d6363d8838cf133ed6828
Tushar Agarwal <76201310+Tushar49@users.noreply.github.com>
2021-02-12 00:34:52 +0530

    Started menu.h

--- main.c
+++ main.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "menu.h"
 #include "timeGap.h"
+char menu[]="HelloWorld";
+
 int main()
 {
-        printf("\033[1m\t\t\033[7mWELCOME TO PUZZLES!!\033[0m\n");
-        timegap(10);
+        char menuOptions[255][255]={
+        {"       Starters"},
+        {"     North Indian"},
+        {"     South Indian"},
+        {"      Continental"},        //French, Spanish and Italian cuisine
+        {"      Sandwiches"}, 
+        {"       Desserts"},
+        {"     Hot Beverages"},
+        {"Cocktails and Soft Drinks"}
+};
+
+        printf("\033[1m\t\t\t\033[7mWELCOME TO PUZZLES!!\033[0m\n\n\n");
+        //timegap(10);
+        upperLayerMenu(menuOptions);
 }
--- menu.h
+++ menu.h
@@ -1,8 +1,16 @@
-void upperLayerMenu()
+void upperLayerMenu(char menuOptions[255][255])
 {
     // enum countryFood {Starters= 1, Indian, Chinese, French, Pasta, Pizzas, Drinks};
     // enum countryFood n1;
     // scanf("%[a,b,c,d,....,z]", n1);
     // printf("Your choice is: %s", n1);
+    // printf("%d", strlen(menuOptions));
+    printf("************************************************************\n");
+    printf("\033[7m\t\t\t      MENU      \t\t\t\033[0m\n");
+    printf("************************************************************\n");
     
+    for(int i=0; i<strlen(menuOptions); i++)
+    {
+        printf("\t       %d.%s\n",(i+1), menuOptions[i]);
+    }
 }
