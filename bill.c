349494bec93de16de20942d8539dfa222d99860f
Tushar Agarwal <76201310+Tushar49@users.noreply.github.com>
2021-02-15 20:48:19 +0530

    Updates
    Removed ----- bill.c
    (added its call function to the main.c)
    Finished ------menuOptions funtion
    Created ------startersMenu function

--- bill.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-#include "bill.h"
-int main()
-{
-    displayBill();
-
-}
--- menu.h
+++ menu.h
@@ -1,6 +1,6 @@
-void upperLayerMenu(char menuOptions[8][255])
+int upperLayerMenu(char menuOptions[8][255])
 {
-    // enum countryFood {Starters= 1, Indian, Chinese, French, Pasta, Pizzas, Drinks};
+    // enum countryFood {STARTERS, NORTH INDIAN,SOUTH INDIAN,SANDWICHES,DESSERTS,HOT BEVERAGES,COCKTAILS AND SOFT DRINKS};
     // enum countryFood n1;
     // scanf("%[a,b,c,d,....,z]", n1);
     // printf("Your choice is: %s", n1);
@@ -11,6 +11,32 @@ void upperLayerMenu(char menuOptions[8][255])
     
     for(int i=0; i<8; i++)
     {
-        printf("\t       %d.%s\n",(i+1), menuOptions[i]);
+        printf("\t\t  %d.%s\n",(i+1), menuOptions[i]);
+    }
+    int choiceMenu;
+    printf("Select the menu category you want to see: ");
+    scanf("%d", &choiceMenu);
+    return choiceMenu;
+}
+
+void startersMenu()
+{
+    char startersItemsNames[][255]={
+        {"Kakori Kebabs------------------Veg "},
+        {"Stir Fried Chilli Chicken------Nveg"},
+        {"Microwave Paneer Tikkas--------Veg "},
+        {"Aloo and Dal ki Tikki----------Veg "},
+        {"Cheese Balls-------------------Veg "},
+        {"Chicken Satay------------------NVeg"},
+        {"Bhuna Masala Chicken Wings-----NVeg"},
+        {"Tangri Kebabs------------------NVeg"},
+        {"Momos--------------------------Veg "},
+        {"Steamed Wontons----------------Veg "}
+    };
+    float startersItemsCost[]={130, 340, 200, 140, 240, 300, 360, 310, 120, 150};
+
+    for(int i=0; i<10; i++)
+    {
+        printf("\t%d.%s\t\t%.2f\n", (i+1), startersItemsNames[i], startersItemsCost[i]);
     }
 }
