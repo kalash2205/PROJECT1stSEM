9947130829d5aad7df5f979f7d963b2c29badd35
kalash <singhal.kalash22@gmail.com>
2021-02-18 19:14:16 +0530

    updated menu

--- main.c
+++ main.c
@@ -21,6 +21,18 @@ int main()
             "COCKTAILS AND SOFT DRINKS",
         };
         char resultOrder[255][255];
+        int x=0;
+        char menuOptions[8][255] ={
+                "STARTERS",
+                "NORTH INDIAN",
+                "SOUTH INDIAN",
+                "CONTINENTAL", //French, Spanish and Italian cuisine
+                "SANDWICHES",
+                "DESSERTS",
+                "HOT BEVERAGES",
+                "COCKTAILS AND SOFT DRINKS",
+            };
+        char resultOrder[255][255];           
         printf("\033[1m\t\t\t\033[7mWELCOME TO PUZZLES!!\033[0m\n\n\n");
 initialMenuBegin:
         printf("\n");
