887423051fcbb3a124898224a62464897f825f1a
Tushar Agarwal <76201310+Tushar49@users.noreply.github.com>
2021-02-22 13:32:28 +0530

    Changed Date printing, 10'%' display discount  fixed

--- bill.h
+++ bill.h
@@ -1,4 +1,3 @@
-
 void displayBill(char resultOrder[255][255], int quantity[100], int price[100], int *x, cust id, int discountChoice)
 {
     float subtotal = 0;
@@ -14,25 +13,26 @@ void displayBill(char resultOrder[255][255], int quantity[100], int price[100], 
     int i=(rand()%10);
     time_t t;
     time(&t);
+    char date[25];
+    strncpy(date, ctime(&t), 24);
     //printf("\t\t*************************************************************************************************\n");
-    printf("\t\t __________________________________________________________________________________________________\n");
+    printf("\t\t|oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo|\n");
     printf("\t\t|                                           P U Z Z L E S                                          |\n");
     printf("\t\t|                                            (address)                                             |\n");
     printf("\t\t|                                              phone                                               |\n");
     printf("\t\t|                                              website                                             |\n");
-    printf("\t\t|Date and Time:%s                                                                                     ", ctime(&t));
-    printf("\t\t|                                                                    Bill no. :%d\n        |", billno);
-    printf("\t\t|Table no. : %d                                                              Waiter:%s              \n", tableno, waiter[i]);
+    printf("\t\t|Date and Time:%s                                              Bill no. :%d|\n", date, billno);
+    printf("\t\t|Table no. : %d                                                              Waiter:%0*s              \n", tableno, 10, waiter[i]);
     printf("\t\t|Customer Name:  %s                                                                                 \n", id.NAME);
     printf("\t\t|Customer Phone: %d                                                                                 \n", id.PHONE);
 
 
-    printf("\t\t|**************************************************************************************************|\n");
+    printf("\t\t|oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo|\n");
     printf("\t\t|                                             DINE IN                                              |\n");
-    printf("\t\t|**************************************************************************************************|\n");
-    printf("\t\t|__________________________________________________________________________________________________|\n");
+    printf("\t\t|oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo|\n");
+    printf("\t\t|oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo|\n");
     printf("\t\t|ITEM                                    PRICE(in Rs.)              QUANTITY         AMOUNT(in Rs.)|\n");
-    printf("\t\t|__________________________________________________________________________________________________|\n");
+    printf("\t\t|oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo|\n");
     for (int i = 0; i < *x; i++)
     {
         if(quantity[i]<10)
@@ -41,7 +41,7 @@ void displayBill(char resultOrder[255][255], int quantity[100], int price[100], 
             printf("\t\t%s         %d                      %d                   %d", resultOrder[i], price[i], quantity[i], quantity[i] * price[i]);   
     printf("\n");
     }
-    printf("\t\t|__________________________________________________________________________________________________|\n");
+    printf("\t\t|oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo|\n");
     for (int i = 0; i < *x; i++)
     {
         subtotal += quantity[i] * price[i];
@@ -66,7 +66,7 @@ void displayBill(char resultOrder[255][255], int quantity[100], int price[100], 
     printf("\t\t|GRAND TOTAL:                                                                          %d.00       \n", (int)grandtotal);
     printf("\t\t|==================================================================================================|\n");
     printf("\t\t|                                        T H A N K    Y O U                                        |\n");
-    printf("\t\t|**************************************************************************************************|\n");
-    printf("\t\t|__________________________________________________________________________________________________|\n");
+    printf("\t\t|oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo|\n");
+    printf("\t\t|oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo|\n");
 
 }
--- main.c
+++ main.c
@@ -241,7 +241,7 @@ placedOrder:
         {
                 gameDiscountOffer=guessGame();
                 if(gameDiscountOffer==1)
-                printf("For winning the game, we have applied a discount of 10\% to your order!\n");
+                printf("For winning the game, we have applied a discount of 10%c to your order!\n", '%');
         }
         else
         {
